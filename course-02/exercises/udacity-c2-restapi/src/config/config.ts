4d5a36d7bd0079ed15b6ac9a2e6ca5744eadd497
rashad <rashadmad@gmail.com>
2019-11-13 21:34:12 -0600

    intial commit

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "rashad",
+    "password": "password",
+    "database": "UdagramDB",
+    "host": "udagram.ch8gy7qyddah.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_reigion": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-filestore"
   },
   "prod": {
     "username": "",
