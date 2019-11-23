0006f286bc26820dd7ef7fade9dd0c8611f8108f
rashad <rashadmad@gmail.com>
2019-11-23 11:44:55 -0600

    create test for url validation

--- src/test/unit.test.ts
+++ src/test/unit.test.ts
@@ -4,5 +4,10 @@ import { expect } from 'chai';
 import 'mocha';
 
 describe('Validate url', () => {
-
+    
+    it('should recieve a url', (urlToBeValidated) => {
+        urlToBeValidated
+    })
+    
+    const result = urlReceived()
 });
